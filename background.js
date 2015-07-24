3e23654024c22b6672250ae910e9b88aae2ee741
Aadarsh Kenia <aadarsh.kenia@stonybrook.edu>
2015-07-24 01:06:14 -0400

    Time tracking Phase 1 Commit

--- background.js
+++ background.js
@@ -1,36 +1,75 @@
 
-var fb_url = "facebook.com";
-chrome.tabs.onCreated.addListener(function foo(tab){   
-    //chrome.tabs.update(tab.tabId, {active : true, url: "display.html"}, function foo(){
-    //});   
-});
 
+//Mappings from tabId to url
+var mappings = {};
+
+//Mappings from url to time spend
+var time_spent = {};
+
+//ArrayList of tracked url s
+var tracked_urls = [];
+var last_short_url=null;
+var cur_start=0;
+var cur_end=0;
+
+add_url_to_tracked("facebook.com");
+add_url_to_tracked("stackoverflow.com");
+
+chrome.tabs.onActivated.addListener(function newTabSelection(activeInfo){
+   console.log("Tab change recorded");
+    if(last_short_url!=null){
+    //Set end time of previously selected tab
+    cur_end = new Date().getTime();
+    var diff = cur_end - cur_start;
+    var cur_spent = time_spent[last_short_url];
+    cur_spent+=diff;
+    time_spent[last_short_url]=cur_spent;    
+    console.log("time_spent:"+last_short_url+" :"+cur_spent);
+    last_short_url=null;
+   }
+   
+});
 
 chrome.tabs.onUpdated.addListener(function bar(tabId, changeInfo, tab){
     var url=tab.url;
     if(url){
-    if(strContains(url, fb_url)){
-           
+        //Check if whether this url is list of tracked urls.
+        var index = isTracked(url);
+        if(index != -1){
+            mappings[tabId.toString()]=url; 
+            last_short_url = tracked_urls[index];
+            //Set start time of access            
+            cur_start=new Date().getTime();
+        }
+        else
+            last_short_url=null;
     }
-     }
 });
 
-chrome.tabs.onRemoved.addListener(function cleanup(tabId, removeInfo){
-    //Cannot access tab url as this has been already deleted.
-    console.log("REMOVED: "+tabId);
-});
 
-function incrementFbCount(){
-    var count = parseInt(localStorage.getItem("fbcount"));
-    if(count==null)
-        count=0;    
-    count+=1;
-    localStorage.setItem("fbcount", count.toString());
-}
-
+//Returns true if b is a substring of a
 function strContains(a, b)
 {
     if(a.indexOf(b) > -1)
         return true;
     return false;
+}
+
+//Function to add a url to tracked_url list and intialize times to zero
+function add_url_to_tracked(url){
+    tracked_urls.push(url);
+    time_spent[url] = 0;
+}
+
+function remove_url_from_tracked(){
+    tracked_urls.pop();
+}
+
+function isTracked(url){
+    for(i=0;i<tracked_urls.length;i++){
+        if(url.indexOf(tracked_urls[i])>-1){
+            return i;
+        }
+    }
+    return -1;
 }
