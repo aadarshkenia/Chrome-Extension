25e4de4654bc937b27bbdd103dbaab7ac950597a
Aadarsh Kenia <aadarsh.kenia@stonybrook.edu>
2015-08-17 11:11:48 -0400

    Project Tracking Working: Display pending

--- popup.js
+++ popup.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-	//display_projects();
-	document.getElementById("btn_categories").addEventListener("click", function (e) {
+	display_curr_proj();
+	//var temp = getArray("Test1.D1");
+        //if(temp)
+        //    alert(temp[0]);
+        document.getElementById("btn_categories").addEventListener("click", function (e) {
 		
 		var create_filter={url: chrome.extension.getURL("categories.html"), active:true};
 		chrome.tabs.create(create_filter);
@@ -15,48 +18,93 @@ document.addEventListener('DOMContentLoaded', function(){
 			
 	});
         
-        document.getElementById("btn_stats").addEventListener("click", function(e){
+        document.getElementById("btn_productivity_stats").addEventListener("click", function(e){
                 chrome.tabs.create({url: "stats.html", active: true});
         });
 
 });
 
 
-function display_projects(){
+function display_curr_proj(){
 	
-	// Check browser support
-	if (typeof(Storage) != "undefined") {
-	    var delivs = [];
-
-	    delivs.push("Deliverable 1");
-	    delivs.push("Deliverable 2");
-	    var cp="Project1";
-	    
-	    localStorage.setItem("current_project", cp);
-	    localStorage.setItem(cp, JSON.stringify(delivs));
-
-	    //Get deliverables of current project
-	    var current_proj = localStorage.getItem("current_project");
+        //Get deliverables of current project
+        var current_proj = localStorage.getItem("current_project");
     	if(current_proj){
-    		var json_deliverables = JSON.parse(localStorage.getItem(cp));			    
-    		var i=0;
-    		for(i=0;i<json_deliverables.length;i++){
-    			var item = json_deliverables[i];
-    			
-    			var para = document.createElement("p");
-                        var node = document.createTextNode(item);
-                        para.appendChild(node);
-                        document.body.appendChild(para);
-    		
-    		}    		
+            document.getElementById("proj_name").innerHTML=current_proj;	
+            var delivs = getArray(current_proj);
+            if(delivs){
+                var i=0;
+                for(i=0;i<delivs.length;i++){
+                    if(i==2)
+                        alert(delivs[i]);    
+                    display_deliverable(delivs[i], current_proj);
+                }
+            }            
     	}
-    	else
-    		alert("No project selected as active project.");	    
+    	else{
+    		alert("No active project");	    
 	} 
-	else {
-		alert("Error");
-	}
+}
 
-	
-	 
+function display_deliverable(d_name, p_name){
+    var name = p_name+"."+d_name;
+    var d_arr = getArray(name);
+    if(!d_arr)
+        alert("No details found for this deliverable!");
+    else{
+        var item = document.createElement("li");
+        var text_el = document.createElement("p");
+        text_el.innerHTML  = d_name;
+        var button_el = document.createElement("button");
+        button_el.innerHTML=toggle(d_arr[0]);
+        button_el.setAttribute("id",name);
+        item.appendChild(text_el);
+        item.appendChild(button_el);
+        document.getElementById("deliverables").appendChild(item);
+        
+        //On click listener for button element
+        document.getElementById(name).addEventListener("click", function(e){
+            deliverableClickHandler(name);
+        });
+    }
+    
+}
+
+function deliverableClickHandler(name){
+    var arr = getArray(name);
+    
+    if(arr){
+        if(arr[0]==="stop"){
+           
+            arr[1]= new Date().getTime();//set last access time
+            arr[0]="start";
+            setArray(name, arr);
+        }
+        else{
+            var time_spent = new Date().getTime() - arr[1];
+            arr[0]="stop";
+            arr[1]=0;
+            arr[2]+=time_spent;
+            setArray(name,arr);
+        }
+    }        
+}
+
+function toggle(status){
+    if(status==="start")
+        return "stop";
+    if(status==="stop")
+        return "start";
+    alert("Wrong status");
+}
+
+//Get array from localStorage after deserialization
+function getArray(key){
+    var arr = JSON.parse(localStorage.getItem(key));
+    return arr;
+}
+
+//Set array to localStorage after serialization
+function setArray(key, arr){
+    localStorage.setItem(key, JSON.stringify(arr));
 }
--- projects.js
+++ projects.js
@@ -1,23 +1,149 @@
+var DEFAULT_STATUS = "stop"; //default status for deliverable
+var DEFAULT_LAT=0;//default last access time
+var DEFAULT_DUR=0;//default total time
+
+document.addEventListener('DOMContentLoaded', function(){
+    
+    create_project_dropdown("change_project_dropdown");
+    create_project_dropdown("add_deliverable_project_dropdown");
+    create_project_dropdown("remove_project_dropdown");
+    
+    //Listener for adding new project
+    document.getElementById("btn_new_project").addEventListener("click", function (e) {	
+	var project_name = document.getElementById("text_new_project").value;	
+        //Get JSON string if it exists
+        var projects = localStorage.getItem("project_list");
+        
+        if(!projects){
+            var project_list = [];
+            project_list.push(project_name);
+            setArray("project_list", project_list);
+            alert("Added "+project_name+" to Projects.");
+        }
+        else
+            addToArray("project_list", project_name);
+    });
+
+    //Listener for adding new deliverable
+    document.getElementById("btn_new_deliverable").addEventListener("click", function (e) {	
+        var deliv_name = document.getElementById("text_new_deliverable").value;
+        var dropdown = document.getElementById("add_deliverable_project_dropdown");
+        var proj_name = dropdown.options[dropdown.selectedIndex].text;
+        addDeliverable(deliv_name, proj_name);
+    });
+    
+    //Listener for changing curr project
+    document.getElementById("btn_track_project").addEventListener("click", function (e) {
+        var dropdown = document.getElementById("change_project_dropdown");
+        var proj_name = dropdown.options[dropdown.selectedIndex].text;
+        localStorage.setItem("current_project", proj_name);        
+    });    
+    
+    //Listener for removing a project
+    document.getElementById("btn_remove_project").addEventListener("click", function(e){
+        var dropdown = document.getElementById("remove_project_dropdown");
+        var proj_name = dropdown.options[dropdown.selectedIndex].text;
+        if(proj_name)
+            remove_project(proj_name);
+    });
+
+});
+
+function addDeliverable(deliv, proj){
+    var delivs = getArray(proj);
+    if(!delivs)
+        delivs = [];
+    delivs.push(deliv);
+    setArray(proj, delivs);
+    init_deliverable(deliv, proj);
+    alert("Added Deliverable "+deliv+" to Project "+proj);
+}
+
+//Create array for deliverable and set to defaults
+function init_deliverable(deliv, proj){
+    var dname = proj + "." + deliv;
+    var darr = [];
+    darr.push(DEFAULT_STATUS);//status
+    darr.push(DEFAULT_LAT);//last access time
+    darr.push(DEFAULT_STATUS);//total duration
+    setArray(dname, darr);
+}
 
 
-
-
-
-
-
-
-
-
-
+function remove_project(proj_name){
+    //Remove proj and all its deliverables
+    if(proj_name===localStorage.getItem("current_project"))
+        localStorage.removeItem("current_project");
+    var projs = getArray("project_list");
+    if(projs){
+        var newarr = [];
+        var i=0;
+        for(i=0;i<projs.length;i++){
+            if(projs[i]!=proj_name)
+                newarr.push(projs[i]);
+        }
+        setArray("project_list", newarr);
+        alert("Removed "+proj_name);
+    }
+    //Delete each deliverable
+    var delivs = getArray(proj_name);
+    if(delivs){
+        var j=0;
+        for(j=0;j<delivs.length;j++){
+            localStorage.removeItem(proj_name+"."+delivs[i]);
+        }
+        
+        localStorage.removeItem(proj_name);//del delivs array
+    }    
+}
 
 //Dropdown of all tracked sites
-function create_project_dropdown(){
+function create_project_dropdown(dropdown_id){
     var projs = getArray("project_list");
-    var dropdown = document.getElementById("project_dropdown");
-    var i=0;
-    for(i=0;i<projs.length;i++){
-        var option = document.createElement("option");
-        option.text = projs[i];
-        dropdown.add(option);        
+    if(projs){
+        var dropdown = document.getElementById(dropdown_id);
+        var i=0;
+        for(i=0;i<projs.length;i++){
+            var option = document.createElement("option");
+            option.text = projs[i];
+            dropdown.add(option);        
+        }
+    }    
+}
+
+//Get array from localStorage after deserialization
+function getArray(key){
+    var arr = JSON.parse(localStorage.getItem(key));
+    return arr;
+}
+
+//Set array to localStorage after serialization
+function setArray(key, arr){
+    localStorage.setItem(key, JSON.stringify(arr));
+}
+
+function addToArray(key, newval){
+    var arr = getArray(key);
+    if(arr){
+        arr.push(newval);
+        setArray(key,arr);
+        alert("Added "+newval);
     }
+    else{
+        //Create new array and push val
+        arr = [];
+        arr.push(newval);
+        setArray(key,arr);
+    }
+}
+
+function removeFromArray(key, val){
+    var arr = getArray(key);
+    var newarr = [];
+    var i = 0;
+    for(i=0;i<arr.length;i++){
+        if(arr[i]!==val)
+            newarr.push(arr[i]);
+    }
+    setArray(key, newarr);
 }
